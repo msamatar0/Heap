c6d72d02349e6ba0593e957940b3aaa7e214eddc
msamatar0 <mohamedsamatar100@gmail.com>
2017-03-09 13:59:24 -0800

    Add files via upload

---
+++ a6.cpp
@@ -0,0 +1,33 @@
+/*
+* Mohamed Samatar - 101848
+* Assignment 6 - Heap Sort
+* Description - Uses a heap to sort an array of key-value pairs
+*/
+#include "heap.h"
+
+int main(){
+	cout << "/*\n * Mohamed Samatar - 101848\n * Assignment 6 - Heap Sort\n"
+		<< " * Description - Uses a heap to sort an array of key-value pairs\n */\n\n";
+	node nodes[10]{
+		node(33, "Rams"),
+		node(22, "Vikings"),
+		node(88, "Lions"),
+		node(44, "Dolphins"),
+		node(123, "Patriots"),
+		node(44, "Bengals"),
+		node(69, "Packers"),
+		node(24, "Broncos"),
+		node(13, "Bears"),
+		node(49, "Chargers")
+	};
+	heap<node> nodeHeap(nodes, 10);
+	cout << nodeHeap << endl << endl;
+	for(int i = 0; i < 10; ++i){
+		nodes[i] = nodeHeap.top();
+		nodeHeap.remove();
+		cout << nodeHeap << endl << endl;
+	}
+	cout << "Sorted Heap:\n";
+	for(int i = 0; i < 10; ++i)
+		cout << nodes[i] << endl;
+}
---
+++ heap.h
@@ -0,0 +1,118 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <queue>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+struct node{
+	int key;
+	string value;
+	node(int k = 0, string v = ""): key(k), value(v){}
+	bool operator<(node &n){
+		return key < n.key;
+	}
+	bool operator>(node &n){
+		return key > n.key;
+	}
+	friend ostream &operator<<(ostream &nout, node &n){
+		return nout << n.key << ", " << n.value;
+	}
+};
+
+//Vector heap
+template<typename t> class heap{
+	vector<t> vecHeap;
+	//Recursively swaps a new element until it fits in heap order
+	void upHeap(int i){
+		if(i == 0)
+			return;
+		int pidx = (i - 1) / 2;
+		if(vecHeap[pidx] > vecHeap[i]){
+			t temp = vecHeap[pidx];
+			vecHeap[pidx] = vecHeap[i];
+			vecHeap[i] = temp;
+			upHeap(pidx);
+		}
+	}
+	void downHeap(int i){
+		int len = vecHeap.size();
+		int lidx = 2 * i + 1;
+		int ridx = 2 * i + 2;
+		if(lidx >= len)
+			return;
+		int min = i;
+		if(vecHeap[i] > vecHeap[lidx])
+			min = lidx;
+
+		if((ridx < len) && (vecHeap[min] > vecHeap[ridx]))
+			min = ridx;
+
+		if(min != i){
+			t temp = vecHeap[i];
+			vecHeap[i] = vecHeap[min];
+			vecHeap[min] = temp;
+			downHeap(min);
+		}
+	}
+protected:
+	//For priority queue: loads all heap elements into a passed-in vector
+	void getVector(vector<t> &v){
+		for(int i = 0; i < size(); ++i)
+			v.push_back(vecHeap[i]);
+	}
+public:
+	heap(){}
+	//size_t used to prevent negative vector size
+	heap(size_t s) : vecHeap(vector<t>(s)){}
+	//constructs heap from array
+	heap(t *a, size_t len){
+		for(int i = 0; i < len; ++i)
+			add(a[i]);
+	}
+	~heap(){}
+	void add(t o){
+		vecHeap.push_back(o);
+		upHeap(size() - 1);
+	}
+	//size = all elements in h
+	int size(){
+		return vecHeap.size();
+	}
+	bool empty(){
+		return size() == 0;
+	}
+	//returns minimum element
+	t top(){
+		return vecHeap[0];
+	}
+	//heap height = log(n)
+	int height(){
+		return empty() ? 0 : log2(size());
+	}
+	//Takes index of left child
+	int left(int i){
+		return 2 * i + 1;
+	}
+	//Takes index of right child
+	int right(int i){
+		return  2 * i + 2;
+	}
+	//Takes index of parent
+	int parent(int i){
+		return i == 0 ? 0 : (i - 1) / 2;
+	}
+	void remove(){
+		vecHeap[0] = vecHeap[size() - 1];
+		vecHeap.pop_back();
+		downHeap(0);
+	}
+	friend ostream &operator<<<t>(ostream &hout, heap &h){
+		hout << '[';
+		for(int i = 0; i < h.size(); ++i)
+			hout << h.vecHeap[i] << (i == h.size() - 1? "" : ", ");
+		return hout << ']';
+	}
+};
